e3117f639c8948898c4e7d965223974b36c11a86
ylgr <quyen.luminous@gmail.com>
2021-06-01 17:31:54 +0700

    (+) add verion icetea

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const {GlitchWeb3} = require('@glitchdefi/web3');
+const {IceteaWeb3} = require('@iceteachain/web3');
 require('dotenv').config();
 const CronJob = require('cron').CronJob;
 
-const gweb3Providers = process.env.BLOCKCHAIN_RPC.split(',').map(e => new GlitchWeb3(e))
+const gweb3Providers = process.env.BLOCKCHAIN_RPC.split(',').map(e => new IceteaWeb3(e))
 let countSpam = 0
 async function faucetAndAddToWallet() {
     for(const gweb3 of gweb3Providers) {
-        gweb3.wallet.importAccount('F28JJMw61xbs7GQ6uhZPrQ7pMUQxZPj7V3E5GW9cMeib') // tglc1a9yfs7e7ev2p7jad9kquwtvxpcznnec422udrk
-        gweb3.wallet.importAccount('J87phy65aXEpTKzZVeWZAZu7j3ooG1qWBvwihM2kduyU') // tglc1lrnqzvplc23hrcn3ndy995xttdm9q6a9d098ht
-        gweb3.wallet.importAccount('Eq5QRGtuaRGBaH2cLQHypwqotiVhAWAEYMrezJeXYHTn') // tglc1acy0cxm7tqdasrvqpdald80qcseyqdcrcw6zlx
+        gweb3.wallet.importAccount('F28JJMw61xbs7GQ6uhZPrQ7pMUQxZPj7V3E5GW9cMeib') // teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5
+        gweb3.wallet.importAccount('J87phy65aXEpTKzZVeWZAZu7j3ooG1qWBvwihM2kduyU') // teat1lrnqzvplc23hrcn3ndy995xttdm9q6a9cx9ref
+        gweb3.wallet.importAccount('Eq5QRGtuaRGBaH2cLQHypwqotiVhAWAEYMrezJeXYHTn') // teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5
     }
     const faucetContract = gweb3Providers[0].contract('system.faucet');
 
     await faucetContract.methods.request().sendCommit({
-        signers: 'tglc1acy0cxm7tqdasrvqpdald80qcseyqdcrcw6zlx',
-        payer: 'system.faucet',
-        fee: 5460000000000n
+        signers: 'teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5',
+        payer: 'system.faucet'
     })
 }
 
@@ -24,17 +23,17 @@ async function boardcastSpammer() {
     console.log('Spam number ', ++countSpam)
 
     let reqs = []
-    const nonce = await gweb3Providers[0].getNonce()
-    const tx = {
-        from: 'tglc1acy0cxm7tqdasrvqpdald80qcseyqdcrcw6zlx',
-        to: 'tglc1lrnqzvplc23hrcn3ndy995xttdm9q6a9d098ht',
-        payer: 'tglc1acy0cxm7tqdasrvqpdald80qcseyqdcrcw6zlx',
-        value: 15000000000000,
-        fee: 5000000000000,
-        nonce: nonce
-    }
+    // const nonce = await gweb3Providers[0].getNonce()
+    // const tx = {
+    //     from: 'teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5',
+    //     to: 'teat1lrnqzvplc23hrcn3ndy995xttdm9q6a9cx9ref',
+    //     payer: 'teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5',
+    //     value: 15000000000000,
+    //     fee: 5000000000000,
+    //     nonce: nonce
+    // }
     for(const gweb3 of gweb3Providers) {
-        reqs.push(gweb3.sendTransaction(tx,'tglc1acy0cxm7tqdasrvqpdald80qcseyqdcrcw6zlx'))
+        reqs.push(gweb3.transfer('teat1lrnqzvplc23hrcn3ndy995xttdm9q6a9cx9ref',15,'teat1a9yfs7e7ev2p7jad9kquwtvxpcznnec4lrufd5'))
     }
     try {
         await Promise.all(reqs)
@@ -50,7 +49,7 @@ async function boardcastSpammer() {
         console.log('Faucet fail: ', e.message)
     }
 
-    const job = new CronJob('6 */37 * * * *', async function() {
+    const job = new CronJob('* * * * * *', async function() {
         await boardcastSpammer();
     }, null, true, 'America/Los_Angeles');
     job.start();
